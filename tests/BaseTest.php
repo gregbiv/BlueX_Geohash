b7728eb23b8f94669f5f5721c7724d3b7d1e2781
beavis <gregbiv@gmail.com>
2014-07-18 18:26:21 +0600

    php 5.3 support

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -22,38 +22,38 @@ abstract class BaseTest extends \PHPUnit_Framework_TestCase
     public static function digitProvider()
     {
         return array(
-            [0, '0'],
-            [1, '1'],
-            [2, '2'],
-            [3, '3'],
-            [4, '4'],
-            [5, '5'],
-            [6, '6'],
-            [7, '7'],
-            [8, '8'],
-            [9, '9'],
-            [10, 'b'],
-            [11, 'c'],
-            [12, 'd'],
-            [13, 'e'],
-            [14, 'f'],
-            [15, 'g'],
-            [16, 'h'],
-            [17, 'j'],
-            [18, 'k'],
-            [19, 'm'],
-            [20, 'n'],
-            [21, 'p'],
-            [22, 'q'],
-            [23, 'r'],
-            [24, 's'],
-            [25, 't'],
-            [26, 'u'],
-            [27, 'v'],
-            [28, 'w'],
-            [29, 'x'],
-            [30, 'y'],
-            [31, 'z']
+            array(0, '0'),
+            array(1, '1'),
+            array(2, '2'),
+            array(3, '3'),
+            array(4, '4'),
+            array(5, '5'),
+            array(6, '6'),
+            array(7, '7'),
+            array(8, '8'),
+            array(9, '9'),
+            array(10, 'b'),
+            array(11, 'c'),
+            array(12, 'd'),
+            array(13, 'e'),
+            array(14, 'f'),
+            array(15, 'g'),
+            array(16, 'h'),
+            array(17, 'j'),
+            array(18, 'k'),
+            array(19, 'm'),
+            array(20, 'n'),
+            array(21, 'p'),
+            array(22, 'q'),
+            array(23, 'r'),
+            array(24, 's'),
+            array(25, 't'),
+            array(26, 'u'),
+            array(27, 'v'),
+            array(28, 'w'),
+            array(29, 'x'),
+            array(30, 'y'),
+            array(31, 'z')
         );
     }
 
@@ -65,27 +65,27 @@ abstract class BaseTest extends \PHPUnit_Framework_TestCase
     public static function encodingProvider()
     {
         // test 12 character Hashes, edge cases and internal places
-        $encodings = [
-            [new Point(0, 0), 12, 's00000000000'],
-            [new Point(45, 90), 12, 'y00000000000'],
-            [new Point(45, -90), 12, 'f00000000000'],
-            [new Point(-45, 90), 12, 'q00000000000'],
-            [new Point(-45, -90), 12, '600000000000'],
-            [new Point(90, 180), 12, 'zzzzzzzzzzzz'],
-            [new Point(90, -180), 12, 'bpbpbpbpbpbp'],
-            [new Point(-90, 180), 12, 'pbpbpbpbpbpb'],
-            [new Point(-90, -180), 12, '000000000000'],
-            [new Point(42.350072, -71.047656), 12, 'drt2zm8ej9eg'],
-            [new Point(38.898632, -77.036541), 12, 'dqcjqcr8yqxd'],
-            [new Point(-23.442503, -58.443832), 12, '6ey6wh6t808q'],
-            [new Point(47.516231, 14.550072), 12, 'u26q7454172n'],
-            [new Point(19.856270, 102.495496), 12, 'w78buqdznjj0'],
-        ];
+        $encodings = array(
+            array(new Point(0, 0), 12, 's00000000000'),
+            array(new Point(45, 90), 12, 'y00000000000'),
+            array(new Point(45, -90), 12, 'f00000000000'),
+            array(new Point(-45, 90), 12, 'q00000000000'),
+            array(new Point(-45, -90), 12, '600000000000'),
+            array(new Point(90, 180), 12, 'zzzzzzzzzzzz'),
+            array(new Point(90, -180), 12, 'bpbpbpbpbpbp'),
+            array(new Point(-90, 180), 12, 'pbpbpbpbpbpb'),
+            array(new Point(-90, -180), 12, '000000000000'),
+            array(new Point(42.350072, -71.047656), 12, 'drt2zm8ej9eg'),
+            array(new Point(38.898632, -77.036541), 12, 'dqcjqcr8yqxd'),
+            array(new Point(-23.442503, -58.443832), 12, '6ey6wh6t808q'),
+            array(new Point(47.516231, 14.550072), 12, 'u26q7454172n'),
+            array(new Point(19.856270, 102.495496), 12, 'w78buqdznjj0'),
+        );
         foreach ($encodings as $test) {
-            $encodings[] = [$test[0], 1, substr($test[2], 0, 1)]; // test single character Hashes
-            $encodings[] = [$test[0], 2, substr($test[2], 0, 2)]; // test two character Hashes
-            $encodings[] = [$test[0], 5, substr($test[2], 0, 5)]; // test five character Hashes
-            $encodings[] = [$test[0], null, substr($test[2], 0, 8)]; // test default eight character Hashes
+            $encodings[] = array($test[0], 1, substr($test[2], 0, 1)); // test single character Hashes
+            $encodings[] = array($test[0], 2, substr($test[2], 0, 2)); // test two character Hashes
+            $encodings[] = array($test[0], 5, substr($test[2], 0, 5)); // test five character Hashes
+            $encodings[] = array($test[0], null, substr($test[2], 0, 8)); // test default eight character Hashes
         }
 
         return $encodings;
@@ -97,33 +97,35 @@ abstract class BaseTest extends \PHPUnit_Framework_TestCase
      */
     public static function HashProvider()
     {
-        $Hashes = [
-            ['s00000000000'],
-            ['y00000000000'],
-            ['f00000000000'],
-            ['q00000000000'],
-            ['600000000000'],
-            ['zzzzzzzzzzzz'],
-            ['bpbpbpbpbpbp'],
-            ['pbpbpbpbpbpb'],
-            ['000000000000'],
-            ['drt2zm8ej9eg'],
-            ['dqcjqcr8yqxd'],
-            ['6ey6wh6t808q'],
-            ['u26q7454172n'],
-            ['w78buqdznjj0']
-        ];
+        $Hashes = array(
+            array('s00000000000'),
+            array('y00000000000'),
+            array('f00000000000'),
+            array('q00000000000'),
+            array('600000000000'),
+            array('zzzzzzzzzzzz'),
+            array('bpbpbpbpbpbp'),
+            array('pbpbpbpbpbpb'),
+            array('000000000000'),
+            array('drt2zm8ej9eg'),
+            array('dqcjqcr8yqxd'),
+            array('6ey6wh6t808q'),
+            array('u26q7454172n'),
+            array('w78buqdznjj0')
+        );
+
         // add some random tests
         for ($i = 0; $i < 3; $i++) {
-            $Hashes[] = [
+            $Hashes[] = array(
                 Hash::encode(new Point(mt_rand(-90000, 90000) / 1000, mt_rand(-180000, 180000) / 1000, 12))
-            ];
+            );
         }
+
         foreach ($Hashes as $test) {
-            $Hashes[] = [substr($test[0], 0, 1)]; // test single character Hashes
-            $Hashes[] = [substr($test[0], 0, 2)]; // test two character Hashes
-            $Hashes[] = [substr($test[0], 0, 5)]; // test five character Hashes
-            $Hashes[] = [substr($test[0], 0, 8)]; // test eight character Hashes
+            $Hashes[] = array(substr($test[0], 0, 1)); // test single character Hashes
+            $Hashes[] = array(substr($test[0], 0, 2)); // test two character Hashes
+            $Hashes[] = array(substr($test[0], 0, 5)); // test five character Hashes
+            $Hashes[] = array(substr($test[0], 0, 8)); // test eight character Hashes
         }
 
         return $Hashes;
